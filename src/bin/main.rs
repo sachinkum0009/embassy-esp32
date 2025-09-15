785e4a1e840facdc17ad6f86c798045c50a87757
Sachin Kumar <sachinkum123567@gmail.com>
2025-09-15 16:07:21 +0200

    initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_hal::{clock::CpuClock, gpio::Output, gpio::OutputConfig, gpio::Level};
+use esp_hal::timer::timg::TimerGroup;
+
+use esp_println as _;
+use defmt::info;
+
+
+// use esp_hal::io::IO;
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+use esp_backtrace as _;
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
+
+    info!("Embassy initialized!");
+
+    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+    let wifi_init = esp_wifi::init(timer1.timer0, rng)
+        .expect("Failed to initialize WIFI/BLE controller");
+    let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
+        .expect("Failed to initialize WIFI controller");
+
+    let output_config = OutputConfig::default();
+    let mut led = Output::new(peripherals.GPIO4, Level::High, output_config);
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world from embassy!");
+        led.toggle();
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+}
